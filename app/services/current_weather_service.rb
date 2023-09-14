f028df8e7095c80b30499881daeb6ef27014a9a8
LAPTOP-CJ75UCV4\Billionaire <macapaz.b1998@gmail.com>
2023-09-14 10:13:13 +0800

    fixing error

--- app/services/current_weather_service.rb
+++ app/services/current_weather_service.rb
@@ -1,1 +1,40 @@
-class CurrentWeatherService  attr_reader :latitude, :longitude, :current_weather, :url, :timezone  def initialize(latitude, longitude, current_weather: true)    @url = "https://api.open-meteo.com/v1/forecast"    @latitude = latitude    @longitude = longitude    @current_weather = current_weather    @daily = 'precipitation_probability_max'    @timezone = 'auto'  end  def call    response = Net::HTTP.get_response(uri)    puts JSON.parse(response.body)    @data = JSON.parse(response.body)  end  private  def uri    return @uri if defined?(@uri)    @uri = URI(url)    params = { latitude: latitude, longitude: longitude, current_weather: current_weather, timezone: timezone, daily: @daily }    @uri.query = URI.encode_www_form(params)    @uri  endend
+class CurrentWeatherService
+  attr_reader :latitude, :longitude, :location_name
+
+  def initialize(latitude, longitude, location_name)
+    @url = "https://api.open-meteo.com/v1/forecast"
+    @latitude = latitude
+    @longitude = longitude
+    @location_name = location_name
+  end
+
+  def call
+    response = Net::HTTP.get_response(uri)
+    data = JSON.parse(response.body)
+    save_location(data) if data['latitude'] && data['longitude']
+    data
+  end
+
+  private
+
+  def uri
+    params = {
+     latitude: latitude,
+     longitude: longitude,
+     current_weather: true,
+     timezone: 'auto',
+     daily: 'precipitation_probability_max'
+   }
+   URI("#{@url}?#{URI.encode_www_form(params)}")
+  end
+
+  def save_location(data)
+    Location.find_or_create_by(name: location_name.to_s.titleize) do |location|
+      location.latitude = data['latitude']
+      location.longitude = data['longitude']
+      location.weather_code = data['current_weather']['weathercode']
+      location.weather_temperature = data['current_weather']['temperature']
+      location.daily_precipitation_max = data['daily']['precipitation_probability_max'][0]
+    end
+  end
+end
