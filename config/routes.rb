d4360678ee63cfcba9714f70d52ea35af3d11f07
LAPTOP-CJ75UCV4\Billionaire <macapaz.b1998@gmail.com>
2023-09-14 10:08:36 +0800

    creating routes for location

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root "home#index"
+  root "locations#index"
+  resources :locations do
+    post 'fetch_weather', on: :collection
+  end
 end
